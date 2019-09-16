4dfb41b26d288e11aaf70f2016737c642606f15e
UGcris <yerme@qq.com>
2019-09-17 00:35:34 +0800

    leetcode 三道题 新的算法：窗口滑动。
    从一个方向往另一个方向，添加数据，直到找到一个重复时，尾巴一个个甩掉直到新添加的数据在原基础上无重复的，依次直至整个字符串循环完。
    
    时间复杂度：O(2n) = O(n)，在最糟糕的情况下，每个字符将被 i 和 j访问两次。
    
    空间复杂度：O(min(m, n))，与之前的方法相同。滑动窗口法需要 O(k)的空间，其中 k表示 Set 的大小。而 Set 的大小取决于字符串 n的大小以及字符集 / 字母m的大小。
    
    Signed-off-by: UGcris <yerme@qq.com>

---
+++ src/main/java/Subject3.java
@@ -0,0 +1,68 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @Author UGcris
+ * @date 2019/9/16
+ **/
+public class Subject3 {
+    /**
+     * 滑动窗口
+     * 从一个方向往另一个方向，添加数据，直到找到一个重复时，尾巴一个个甩掉直到新添加的数据在原基础上无重复的，依次直至整个字符串循环完。
+     */
+    /**
+     * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+     *
+     * 示例 1:
+     *
+     * 输入: "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     * 示例 2:
+     *
+     * 输入: "bbbbb"
+     * 输出: 1
+     * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+     * 示例 3:
+     *
+     * 输入: "pwwkew"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++){
+            for (int j = i + 1 + ans; j <= n; j++){
+                if (allUnique(s, i, j)) {
+                    ans = Math.max(ans, j - i);
+                }else{
+                    break;
+                };
+            }
+            if(i+ans>=n){
+                break;
+            }
+        }
+        return ans;
+    }
+
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<Character>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+
+        return true;
+    }
+}
