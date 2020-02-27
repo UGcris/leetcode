d0613020a7d140fa0bd11ad1ff0f4a02589a520a
UGcris <yerme@qq.com>
2020-02-27 21:51:31 +0800

    107. 二叉树的层次遍历 II.反转正序的list

--- src/main/java/TOTO/BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/TOTO/BinaryTreeLevelOrderTraversalII.java
@@ -3,40 +3,41 @@ package TOTO;
 import TOIN.TreeNode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
  *
- * 例如:
- * 给定二叉树: [3,9,20,null,null,15,7],
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
  *
  *     3
  *    / \
  *   9  20
  *     /  \
  *    15   7
- * 返回其层次遍历结果：
+ * 返回其自底向上的层次遍历为：
  *
  * [
- *   [3],
+ *   [15,7],
  *   [9,20],
- *   [15,7]
+ *   [3]
  * ]
  *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @Author UGcris
- * @date 2020/2/26
+ * @date 2020/2/27
  **/
-public class BinaryTreeLevelOrderTraversal {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+public class BinaryTreeLevelOrderTraversalII {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> ans=new ArrayList<>();
         if(null!=root){
             next(root,ans,0);
         }
+        Collections.reverse(ans);
         return ans;
     }
     private void next(TreeNode root, List<List<Integer>> ans, int index) {
