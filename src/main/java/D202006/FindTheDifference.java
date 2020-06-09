dd934c311e28863c1985a388d4db860b9ca64178
UGcris <yerme@qq.com>
2020-06-09 23:35:05 +0800

    389. 找不同.位运算

---
+++ src/main/java/D202006/FindTheDifference.java
@@ -0,0 +1,39 @@
+package D202006;
+
+/**
+ * 给定两个字符串 s 和 t，它们只包含小写字母。
+ * <p>
+ * 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ * <p>
+ * 请找出在 t 中被添加的字母。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入：
+ * s = "abcd"
+ * t = "abcde"
+ * <p>
+ * 输出：
+ * e
+ * <p>
+ * 解释：
+ * 'e' 是那个被添加的字母。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-the-difference
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author UGcris
+ * @date 2020/6/9
+ **/
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        char ans = t.charAt(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            ans ^= s.charAt(i) ^ t.charAt(i);
+        }
+        return ans;
+    }
+}
