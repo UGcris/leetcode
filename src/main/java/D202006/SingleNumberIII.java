1adef3dc216a0f017f83e15db83690a3ac3f94e8
UGcris <yerme@qq.com>
2020-06-09 23:12:29 +0800

    260. 只出现一次的数字 III.Set

---
+++ src/main/java/D202006/SingleNumberIII.java
@@ -0,0 +1,44 @@
+package D202006;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。
+ * <p>
+ * 示例 :
+ * <p>
+ * 输入: [1,2,1,3,2,5]
+ * 输出: [3,5]
+ * 注意：
+ * <p>
+ * 结果输出的顺序并不重要，对于上面的例子， [5, 3] 也是正确答案。
+ * 你的算法应该具有线性时间复杂度。你能否仅使用常数空间复杂度来实现？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author UGcris
+ * @date 2020/6/9
+ **/
+public class SingleNumberIII {
+    public int[] singleNumber(int[] nums) {
+        int[] ans = new int[2];
+        Set<Integer> set=new HashSet<>();
+        for (int num : nums) {
+            if(set.contains(num)){
+                set.remove(num);
+                continue;
+            }
+            set.add(num);
+        }
+        int index=0;
+        for (int num:set) {
+            ans[index++]=num;
+        }
+        return ans;
+    }
+}
