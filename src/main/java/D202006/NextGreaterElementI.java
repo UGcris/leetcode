0133cc7d9f56810b760fa9d96bd442aea91e9e43
UGcris <yerme@qq.com>
2020-06-23 23:43:08 +0800

    496. 下一个更大元素 I

---
+++ src/main/java/D202006/NextGreaterElementI.java
@@ -0,0 +1,63 @@
+package D202006;
+
+import java.util.*;
+
+/**
+ * 给定两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+ * <p>
+ * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出: [-1,3,-1]
+ * 解释:
+ * 对于num1中的数字4，你无法在第二个数组中找到下一个更大的数字，因此输出 -1。
+ * 对于num1中的数字1，第二个数组中数字1右边的下一个较大数字是 3。
+ * 对于num1中的数字2，第二个数组中没有下一个更大的数字，因此输出 -1。
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
+ * 输出: [3,-1]
+ * 解释:
+ *     对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+ * 对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * nums1和nums2中所有元素是唯一的。
+ * nums1和nums2 的数组大小都不超过1000。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-greater-element-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author UGcris
+ * @date 2020/6/23
+ **/
+public class NextGreaterElementI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        List<Integer> invalidIndex = new ArrayList<>();
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            int val = nums2[i];
+            for (int j = 0; j < invalidIndex.size(); ) {
+                int preVal = invalidIndex.get(j);
+                if (preVal >= val) break;
+                indexMap.put(preVal, val);
+                invalidIndex.remove(j);
+            }
+            invalidIndex.add(val);
+            Collections.sort(invalidIndex);
+            indexMap.put(val, -1);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = indexMap.get(nums1[i]);
+        }
+        return ans;
+    }
+}
