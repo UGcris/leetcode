65919980fa8f02dd55817b468c8ba7e53b659ca8
UGcris <yerme@qq.com>
2020-07-08 21:38:33 +0800

    367. 有效的完全平方数

---
+++ src/main/java/D202007/ValidPerfectSquare.java
@@ -0,0 +1,66 @@
+package D202007;
+
+/**
+ * 给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
+ * <p>
+ * 说明：不要使用任何内置的库函数，如  sqrt。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：16
+ * 输出：True
+ * 示例 2：
+ * <p>
+ * 输入：14
+ * 输出：False
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-perfect-square
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author UGcris
+ * @date 2020/7/3
+ **/
+public class ValidPerfectSquare {
+    /**
+     * 1+3+5+...+(2n-1)=n*n
+     *
+     * @param num
+     * @return
+     */
+    public boolean isPerfectSquare(int num) {
+        int index = 1;
+        while (num > 0) {
+            num -= index;
+            index += 2;
+        }
+        return num == 0;
+    }
+
+    /**
+     * 二分查找
+     *
+     * @param num
+     * @return
+     */
+    public boolean isPerfectSquare2(int num) {
+        if (num < 2) {
+            return true;
+        }
+
+        long left = 2, right = num / 2, x, guessSquared;
+        while (left <= right) {
+            x = left + (right - left) / 2;
+            guessSquared = x * x;
+            if (guessSquared == num) {
+                return true;
+            }
+            if (guessSquared > num) {
+                right = x - 1;
+            } else {
+                left = x + 1;
+            }
+        }
+        return false;
+    }
+}
