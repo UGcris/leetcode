acf4b60d9fe23f979e43758f4dcec078e2cc1bbf
UGcris <yerme@qq.com>
2020-07-15 22:22:34 +0800

    338. 比特位计数

---
+++ src/main/java/D202007/CountingBits.java
@@ -0,0 +1,52 @@
+package D202007;
+
+/**
+ * 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 2
+ * 输出: [0,1,1]
+ * 示例 2:
+ * <p>
+ * 输入: 5
+ * 输出: [0,1,1,2,1,2]
+ * 进阶:
+ * <p>
+ * 给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？
+ * 要求算法的空间复杂度为O(n)。
+ * 你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount）来执行此操作。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/counting-bits
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author UGcris
+ * @date 2020/7/15
+ **/
+public class CountingBits {
+    /**
+     * 状态转移方程
+     * 1、dp[2的n次方]=0
+     * 2、after 2的n次方，dp[2的n次方+m]=dp[2的n次方]+dp[m]
+     * @param num
+     * @return
+     */
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        ans[0] = 0;
+        if (num > 0) ans[1] = 1;
+        if (num > 1) {
+            int base = 1;
+            for (int i = 2; i <= num; i++) {
+                if (base * 2 == i) {
+                    base = i;
+                    ans[i] = 1;
+                    continue;
+                }
+                ans[i] = ans[base] + ans[i - base];
+            }
+        }
+        return ans;
+    }
+}
